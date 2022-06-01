601cbf1459dc58f950076629792d5dff493d174a
Arianna Allahyar <anallahyar@gmail.com>
2022-06-01 16:25:48 +0200

    MongoDB connected

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 import express from "express";
 import cors from "cors";
+import connectDB from "./config/db.js";
 
 const port = process.env.PORT || 8080;
+
+connectDB();
 
 const app = express();
 
